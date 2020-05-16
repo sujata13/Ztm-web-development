5778cb92448d4d105f77be8decbc4a603f4452d5
sujata13 <36507578+sujata13@users.noreply.github.com>
2020-05-16 20:39:17 +0530

    Create script.js

---
+++ shopping-list/script.js
@@ -0,0 +1,58 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var deleteBtns=document.getElementsByClassName("delete");
+var items = document.getElementsByTagName("li");
+
+for (var i = 0; i < deleteBtns.length; i++) {
+	deleteBtns[i].addEventListener("click",removeParent,false);
+}
+
+function removeParent(evt){
+	evt.target.removeEventListener("click",removeParent,false);
+	evt.target.parentNode.remove();
+}
+
+function getEventTarget(e){
+	e=e|| window.event;
+	return e.target || e.srcElement;
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var btn =document.createElement("button");
+	btn.innerHTML="Delete";
+	btn.onclick=removeParent;
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.innerHTML=li.innerHTML+" ";
+	li.appendChild(btn);
+
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
