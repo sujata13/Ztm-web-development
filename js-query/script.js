3da1deae2e17f647b8093f2e11b5294c3a06e963
sujata13 <36507578+sujata13@users.noreply.github.com>
2020-05-18 20:32:15 +0530

    Add files via upload

---
+++ js-query/script.js
@@ -0,0 +1,47 @@
+var database = [
+    {
+    	username:"andrei",
+    	password:"supersecret"
+    },
+    {
+    	username:"Sally",
+    	password:"123"
+    },
+    {
+    	username:"ingrid",
+    	password:"777"
+    }
+];
+
+var newsFeed = [
+    {
+    	username: "Bobby",
+    	timeline: "So tired from all that learned"
+    },
+    {
+    	username:"Sally",
+    	timeline:"Javascript is soooooo cooool!"
+    }
+];
+
+function isUserValid(username,password){
+	for (var i = 0; i < database.length; i++) {
+	    if( database[i].username==username&& database[i].password== password){
+	    	return true;
+	    } 
+	}
+	return false;
+}
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPromt = prompt("What's your password");
+
+function signIn(username,password){
+	if(isUserValid(username,password)){
+		console.log(newsFeed);
+	} else{
+		alert("Sorry, wrong username and password!");
+	}
+}
+
+signIn(userNamePrompt,passwordPromt);
